d7329b347997b249b7304dc2798f7a01220cb7f0
MeeshoAbhishek <abhishek.khichi@meesho.com>
2025-08-21 16:18:24 +0530

    Change testing base url to actual

--- app/src/main/java/com/abhishek/jarassignment/utils/AppConstants.kt
+++ app/src/main/java/com/abhishek/jarassignment/utils/AppConstants.kt
@@ -2,7 +2,7 @@ package com.abhishek.jarassignment.utils
 
 object AppConstants {
     const val GET_ONBOARDING_DATA = "c/01ee-5979-4ce6-8ff1"
-    const val BASE_URL = "https://f0570933-6358-4c73-8d1e-5884a319757f.mock.pstmn.io"
+    const val BASE_URL = "https://dummyjson.com"
     internal const val API_FAILED_RESPONSE = "API call failed"
     internal const val UNKNOWN_ERROR = "An unknown error occurred"
     internal const val ENTER_DURATION_MS = 900
