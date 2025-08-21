b2da8f7b66f111da8e4c8c23c288a7f54af066ff
MeeshoAbhishek <abhishek.khichi@meesho.com>
2025-08-21 16:15:29 +0530

    Implement LandingActivity

--- app/src/main/java/com/abhishek/jarassignment/LandingActivity.kt
+++ app/src/main/java/com/abhishek/jarassignment/LandingActivity.kt
@@ -3,14 +3,106 @@ package com.abhishek.jarassignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.abhishek.jarassignment.presentation.ui.theme.JarAssignmentTheme
 
 class LandingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JarAssignmentTheme {}
+            JarAssignmentTheme {
+                LandingScreen(
+                    onBackPressed = { onBackPressed() }
+                )
+            }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LandingScreen(
+    onBackPressed: () -> Unit = {},
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier =
+                            Modifier
+                                .offset(x = (-10).dp)
+                                .fillMaxHeight(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = { onBackPressed() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = null,
+                                tint = Color.White
+                            )
+                        }
+
+                        Text(
+                            text = "Landing Page",
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                fontWeight = FontWeight.W700
+                            ),
+                            color = Color.White
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF201929)
+                )
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF201929))
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Landing Page",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.W700
+                ),
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LandingScreenPreview() {
+    JarAssignmentTheme {
+        LandingScreen()
+    }
+}
